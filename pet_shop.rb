507815831672e21cb7ce0df7263a4892b40f7540
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-25 11:06:30 +0100

    another commit just in case

---
+++ pet_shop.rb
@@ -0,0 +1,23 @@
+def pet_shop_name(shop)
+  shop[:name]
+end
+
+def total_cash(shop)
+  shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop,cash)
+  shop[:admin][:total_cash] += cash
+end
+
+def pets_sold(shop)
+  shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, number_sold)
+  shop[:admin][:pets_sold] += number_sold
+end
+
+def stock_count(shop)
+  shop[:pets].length()
+end
